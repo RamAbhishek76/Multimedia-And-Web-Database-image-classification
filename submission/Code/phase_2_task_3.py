9b0ea4d116266944c797976594585a5e450a1808
Abhinav Gorantla <abhinav20016@gmail.com>
2023-10-03 13:09:06 -0700

    LDA Implemented

--- submission/Code/phase_2_task_3.py
+++ submission/Code/phase_2_task_3.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.decomposition import LatentDirichletAllocation
 
 from database_connection import connect_to_mongo
 
@@ -6,19 +7,53 @@ client = connect_to_mongo()
 db = client.cse515_project_phase1
 collection = db.features
 
-cm = []
-k = 400
+print("select one of the features: ")
+print("1. Color Moment\n2.HoG\n3. Layer3\n4. AvgPool\n5. FC")
+feature = int(input("Choose one of the feature space from above: "))
+k = int(input("Enter k value: "))
+print("Select one of the dimensionality reduction methods: ")
+print("1. SVD\n2.NNMF\n3. LDA\n4. K Means")
+dim_red_method = int(input("Choose one from above: "))
+
+feature_names = ['color_moment',
+                 'hog', 'layer3', 'avgpool', 'fc']
+dim_red_names = ["svd", "nnmf", "lda", "kmeans"]
+
+feature_space = []
+user_feature = feature_names[feature - 1]
 for img in collection.find():
     print(img['image_id'])
-    cm.append(np.array(img["color_moment"]).flatten())
+    feature_space.append(np.array(img[user_feature]).flatten())
 
-cm = np.array(cm)
+feature_space = np.array(feature_space)
 
-U, S, VT = np.linalg.svd(cm)
+match dim_red_method:
+    case 1:
+        U, S, VT = np.linalg.svd(feature_space)
 
-U_k = U[:, :k]
-S_k = np.diag(S[:k])
+        U_k = U[:, :k]
+        S_k = np.diag(S[:k])
 
-latent_semantics = np.dot(U_k, S_k)
+        latent_semantics = np.dot(U_k, S_k)
 
-print(latent_semantics)
+        print(latent_semantics)
+
+    case 2:
+        # TODO: Implement NNMF
+        print("NNMF")
+
+    case 3:
+        print("LDA")
+        feature_space = feature_space + abs(np.min(feature_space)) + 1
+        lda = LatentDirichletAllocation(n_components=k, random_state=42)
+        print(feature_space)
+        lda.fit(feature_space)
+
+        for topic_idx, topic in enumerate(lda.components_):
+            print(f'Topic {topic_idx}:')
+            top_integers = [str(i) for i in topic.argsort()[:-10 - 1:-1]]
+            print(' '.join(top_integers))
+            print()
+    case 4:
+        # TODO: Implement K Means
+        print("KMeans")

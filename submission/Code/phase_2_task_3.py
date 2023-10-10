066b149cb00dc07691647525db82b68f260eb55a
Abhinav Gorantla <abhinav20016@gmail.com>
2023-10-10 13:26:37 -0700

    Added latent sematics in csv files

--- submission/Code/phase_2_task_3.py
+++ submission/Code/phase_2_task_3.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.decomposition import LatentDirichletAllocation
 
 from database_connection import connect_to_mongo
@@ -35,8 +36,18 @@ match dim_red_method:
         S_k = np.diag(S[:k])
 
         latent_semantics = np.dot(U_k, S_k)
+        latent_semantics = np.append([[i for i in range(len(latent_semantics[0]))]],
+                                     latent_semantics, axis=0)
 
         print(latent_semantics)
+
+        file_name = "svd_" + str(k) + "_latent_semantics_" + \
+            feature_names[feature - 1] + ".csv"
+        np.savetxt(file_name,
+                   latent_semantics, delimiter=',', fmt='%f')
+        df = pd.read_csv(file_name)
+        header = [i for i in range(len(df))]
+        df.to_csv(file_name, index=True)
 
     case 2:
         # TODO: Implement NNMF
@@ -46,15 +57,25 @@ match dim_red_method:
         print("LDA")
         feature_space = feature_space + abs(np.min(feature_space)) + 1
         lda = LatentDirichletAllocation(
-            n_components=k, random_state=42, max_iter=6)
-        print(feature_space)
-        lda.fit(feature_space)
+            n_components=k, random_state=42, max_iter=10)
 
+        lda.fit(feature_space)
+        topics = []
         for topic_idx, topic in enumerate(lda.components_):
-            print(f'Topic {topic_idx}:')
-            top_integers = [str(i) for i in topic.argsort()[:-10 - 1:-1]]
-            print(' '.join(top_integers))
-            print()
+            topics.append(topic)
+
+        topics = np.append([[i for i in range(len(topics[0]))]],
+                           topics, axis=0)
+
+        print(topics)
+
+        file_name = "lda_" + str(k) + "_latent_semantics_" + \
+            feature_names[feature - 1] + ".csv"
+        np.savetxt(file_name,
+                   topics, delimiter=',', fmt='%f')
+        df = pd.read_csv(file_name)
+        header = [i for i in range(len(df))]
+        df.to_csv(file_name, index=True)
     case 4:
         # TODO: Implement K Means
         print("KMeans")

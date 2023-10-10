6081c59f78d8956a5ccd73cb02b371931a1b6988
Abhinav Gorantla <abhinav20016@gmail.com>
2023-10-10 16:27:29 -0700

    Added user defined funcs for SVD computation

--- submission/Code/phase_2_task_3.py
+++ submission/Code/phase_2_task_3.py
@@ -6,6 +6,51 @@ from sklearn.cluster import KMeans
 
 from database_connection import connect_to_mongo
 
+
+def power_iteration(matrix, num_iterations=1000):
+    """
+    Perform power iteration to find the dominant singular vector of a matrix.
+    """
+    # Initialize a random vector (initial guess for the dominant singular vector)
+    vector = np.random.rand(matrix.shape[1])
+
+    for _ in range(num_iterations):
+        # Compute the product of the matrix and the vector
+        product = matrix @ vector
+
+        # Compute the norm of the product
+        norm = np.linalg.norm(product)
+
+        # Update the vector to be the normalized product
+        vector = product / norm
+
+    return vector, norm
+
+
+def svd(matrix, num_iterations=1000):
+    """
+    Compute the Singular Value Decomposition (SVD) of a matrix using power iteration.
+    """
+    # Compute A^T * A
+    ata = matrix.T @ matrix
+
+    # Find the dominant eigenvector of A^T * A (right singular vector)
+    v, sigma = power_iteration(ata, num_iterations)
+
+    # Compute the singular value
+    singular_value = np.sqrt(sigma)
+
+    # Find the corresponding left singular vector
+    u = matrix @ v / singular_value
+
+    # Stack the left singular vector, singular value, and right singular vector
+    u = u.reshape(-1, 1)
+    singular_value = np.array([singular_value])
+    v = v.reshape(-1, 1)
+
+    return u, singular_value, v.T
+
+
 client = connect_to_mongo()
 db = client.cse515_project_phase1
 collection = db.phase2_features
@@ -33,7 +78,8 @@ feature_space = np.array(feature_space)
 match dim_red_method:
     case 1:
         print("SVD")
-        U, S, VT = np.linalg.svd(feature_space)
+        # U, S, VT = np.linalg.svd(feature_space)
+        U, S, VT = svd(feature_space)
 
         U_k = U[:, :k]
         S_k = np.diag(S[:k])
@@ -57,7 +103,7 @@ match dim_red_method:
         nmf = NMF(n_components=k, init='random', random_state=42)
         W = nmf.fit_transform(feature_space)
         H = nmf.components_
-        
+
         print(W)
 
     case 3:
@@ -87,5 +133,5 @@ match dim_red_method:
         print("KMeans")
         kmeans = KMeans(n_clusters=k, random_state=42)
         kmeans.fit(feature_space)
-        
+
         print(kmeans.cluster_centers_)
--- submission/Code/phase_2_task_4.py
+++ submission/Code/phase_2_task_4.py
@@ -8,9 +8,13 @@ import numpy as np
 
 # Set tensorly backend to PyTorch
 tl.set_backend('pytorch')
-
-dataset = datasets.ImageFolder(root='path_to_Caltech101_dataset', transform=transform)
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=len(dataset), shuffle=False)
+transform = transforms.Compose([
+    transforms.ToTensor(),
+])
+dataset = datasets.ImageFolder(
+    root='path_to_Caltech101_dataset', transform=transform)
+dataloader = torch.utils.data.DataLoader(
+    dataset, batch_size=len(dataset), shuffle=False)
 
 print("select one of the features: ")
 print("1. Color Moment\n2.HoG\n3. Layer3\n4. AvgPool\n5. FC")
@@ -21,12 +25,14 @@ data, labels = next(iter(dataloader))
 
 tensor_data = torch.stack([features, labels.float()], dim=1)
 
+
 def extract_latent_semantics(tensor_data, k):
     factors = parafac(tensor_data, rank=k)
     # Extract label-weight pairs from the factors
     # This assumes the third mode corresponds to labels
     label_weights = factors[1]
     return label_weights
+
 
 k = 5  # or any other value you choose
 latent_semantics = extract_latent_semantics(tensor_data, k)

da25aba0978143e7f449a2d35534c3252338bd80
sammy-16 <rohansamueljustin@gmail.com>
2023-10-12 19:21:35 -0700

    Added code to make labels make sense from db

--- submission/Code/phase_2_task_4.py
+++ submission/Code/phase_2_task_4.py
@@ -5,24 +5,6 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import csv
 import numpy as np
-from database_connection import connect_to_mongo
-
-
-def extract_latent_semantics(tensor_data, k):
-    factors = parafac(tensor=tensor_data, rank=k)
-    # Extract label-weight pairs from the factors
-    # This assumes the third mode corresponds to labels
-    # This assumes the third mode corresponds to labels
-    print("factors", len(factors))
-    print("factors", len(factors[0]))
-    print("factors", len(factors[0][0]))
-    label_weights = factors[1]
-    return label_weights
-
-
-client = connect_to_mongo()
-db = client.cse515_project_phase1
-collection = db.phase2_features
 
 def save_to_file(semantics, filename):
     with open(filename, 'w', newline='') as csvfile:
@@ -42,15 +24,15 @@ def connect_to_mongo():
         print(e)
     return client
 
-def construct_three_modal_tensor(feature_space, labels, num_labels):
+def construct_three_modal_tensor(feature_space, labels, label_names):
     number_of_images = len(feature_space)
     feature_length = len(feature_space[0])
+    num_labels = len(label_names)
     tensor = torch.randn((number_of_images, feature_length, num_labels), dtype=torch.float32)
     
     for i, (feature_vector, label) in enumerate(zip(feature_space, labels)):
-        #print(f"Label: {label}, Feature Vector: {feature_vector}")
-        normalized_label = max(0, min(label, num_labels - 1))
-        tensor[i, :, normalized_label] = torch.tensor(feature_vector, dtype=torch.float32)
+        label_index = label_names.index(label)
+        tensor[i, :, label_index] = torch.tensor(feature_vector, dtype=torch.float32)
     
     return tensor
 
@@ -61,7 +43,6 @@ def extract_latent_semantics(tensor_data, k, label_names):
     top_k_semantics = []
     for i in range(k):
         semantic_weights = label_factors[:, i]
-        print(f"Label names: {label_names}, Semantic Weights: {semantic_weights}")
         label_weight_pairs = list(zip(label_names, semantic_weights))
         sorted_pairs = sorted(label_weight_pairs, key=lambda x: x[1], reverse=True)
         top_k_semantics.append(sorted_pairs)
@@ -78,33 +59,24 @@ print("Select one of the features: ")
 print("1. Color Moment\n2. HoG\n3. Layer3\n4. AvgPool\n5. FC")
 feature = int(input("Choose one of the feature space from above: "))
 k = int(input("Enter k value: "))
-feature_names = ['color_moment',
-                 'hog', 'layer3', 'avgpool', 'fc']
-feature_space = []
-user_feature = feature_names[feature - 1]
-for img in collection.find():
-    print(img['image_id'])
-    feature_space.append(np.array(img[user_feature]).flatten())
 
 feature_names = ['color_moment', 'hog', 'layer3', 'avgpool', 'fc']
 user_feature = feature_names[feature - 1]
 
 feature_space = []
 labels = []
+labels_set = set()
 
 for img in collection.find():
     feature_space.append(np.array(img[user_feature]).flatten())
     labels.append(img['target'])
+    labels_set.add(img['target'])
 
-num_labels = len(set(labels))
-print(num_labels)
-tensor_data = construct_three_modal_tensor(feature_space, labels, num_labels)
+label_names = sorted(list(labels_set))
+tensor_data = construct_three_modal_tensor(feature_space, labels, label_names)
 
 if torch.cuda.is_available():
     tensor_data = tensor_data.cuda()
-
-# Assuming you have a list of label names corresponding to the integer labels
-label_names = [f"Label_{i}" for i in range(num_labels)]
 
 latent_semantics = extract_latent_semantics(tensor_data, k, label_names)
 file_name = f"latentsemantics_{k}_{user_feature}.csv"

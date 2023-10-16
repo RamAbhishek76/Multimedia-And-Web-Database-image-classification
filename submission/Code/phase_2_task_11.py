b54b1e8d075b2a5416afbb4cf47551a4c2b0326a
sammy-16 <rohansamueljustin@gmail.com>
2023-10-15 19:18:15 -0700

    Added Driver code and output plotter for task 11

--- submission/Code/phase_2_task_11.py
+++ submission/Code/phase_2_task_11.py
@@ -1,6 +1,15 @@
 import networkx as nx
 import numpy as np
 from database_connection import connect_to_mongo
+from output_plotter import output_plotter_task_11
+from torchvision import datasets, transforms
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+dataset = datasets.Caltech101(root='./data', download=True, transform=transform)
 
 def fetch_data_from_db(descriptor, is_latent_space):
     """
@@ -69,10 +78,9 @@ def main(descriptor, n, m, label, is_feature_model):
     data = fetch_data_from_db(descriptor, is_feature_model)
     G = create_similarity_graph(data, n)
     significant_images = personalized_page_rank(G, label, m, data)
-    
+    output_plotter_task_11(dataset=dataset, feature_descriptor=descriptor, label=label, input_image_id_list=list(significant_images))
     return significant_images
 
-# Example usage
 feature_or_ls = input("Do you want to enter a feature Model? (Y/N): ")
 is_latent_space = True if feature_or_ls.lower() == 'n' else False
 if is_latent_space:
@@ -88,8 +96,7 @@ if is_latent_space:
         print("1. SVD\n2.NNMF\n3. LDA\n4. K Means")
         dim_red_method = int(input("Choose one from above: "))
 
-        feature_names = ['color_moment',
-                        'hog', 'layer3', 'avgpool', 'fc']
+        feature_names = ['color_moment', 'hog', 'layer3', 'avgpool', 'fc']
         dim_red_names = ["svd", "nnmf", "lda", "kmeans"]
 
         feature_space = []
@@ -97,15 +104,48 @@ if is_latent_space:
         method = dim_red_names[dim_red_method-1]
         descriptor = (k, user_feature, method)
     elif ls == 2:
-        pass
+        print("select one of the features: ")
+        print("1. Color Moment\n2.HoG\n3. Layer3\n4. AvgPool\n5. FC")
+        feature = int(input("Choose one of the feature space from above: "))
+        k = int(input("Enter k value: "))
+        feature_names = ['color_moment', 'hog', 'layer3', 'avgpool', 'fc']
+        user_feature = feature_names[feature - 1]
+        descriptor = (k, user_feature)
     elif ls == 3:
-        pass
+        print("select one of the features: ")
+        print("1. Color Moment\n2.HoG\n3. Layer3\n4. AvgPool\n5. FC")
+        feature = int(input("Choose one of the feature space from above: "))
+        k = int(input("Enter k value: "))
+        print("Select one of the dimensionality reduction methods: ")
+        print("1. SVD\n2.NNMF\n3. LDA\n4. K Means")
+        dim_red_method = int(input("Choose one from above: "))
+
+        feature_names = ['color_moment', 'hog', 'layer3', 'avgpool', 'fc']
+        dim_red_names = ["svd", "nnmf", "lda", "kmeans"]
+
+        feature_space = []
+        user_feature = feature_names[feature - 1]
+        method = dim_red_names[dim_red_method-1]
+        descriptor = (k, user_feature, method)
     elif ls == 4:
-        pass
+        print("select one of the features: ")
+        print("1. Color Moment\n2.HoG\n3. Layer3\n4. AvgPool\n5. FC")
+        feature = int(input("Choose one of the feature space from above: "))
+        k = int(input("Enter k value: "))
+        print("Select one of the dimensionality reduction methods: ")
+        print("1. SVD\n2.NNMF\n3. LDA\n4. K Means")
+        dim_red_method = int(input("Choose one from above: "))
+
+        feature_names = ['color_moment', 'hog', 'layer3', 'avgpool', 'fc']
+        dim_red_names = ["svd", "nnmf", "lda", "kmeans"]
+
+        feature_space = []
+        user_feature = feature_names[feature - 1]
+        method = dim_red_names[dim_red_method-1]
+        descriptor = (k, user_feature, method)
     else:
         print("Invalid input")
         exit()
-    user_feature = ""
 else:
     print("Enter the feature model: ")
     print("1. Color Moment\n2. HoG\n3. Layer3\n4. AvgPool\n5. FC")
